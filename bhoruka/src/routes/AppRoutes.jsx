d97507de5ee19ca2e6d8b5254d73888e4aa813e8
choudurivenkatesh250-prog <choudurivenkatesh250@gmail.com>
2026-09-02 12:50:37 +0530

    did some work in approutes.jsx

--- bhoruka/src/routes/AppRoutes.jsx
+++ bhoruka/src/routes/AppRoutes.jsx
@@ -6,7 +6,8 @@ const Home = lazy(() => import('../pages/Home'));
 const HydroPower = lazy(() => import('../pages/operations/HydroPower'));
 const SolarPower = lazy(() => import('../pages/operations/SolarPower'));
 const WindPower = lazy(() => import('../pages/operations/WindPower'));
-
+const Marketing = lazy(()=> import('../pages/Marketing'))
+const Clients = lazy(()=> import('../pages/Clients'))
 const AppRoutes = () => {
   return (
     <Suspense fallback={<Loader />}>
@@ -15,6 +16,8 @@ const AppRoutes = () => {
         <Route path="/hydro" element={<HydroPower />} />
         <Route path="/solar" element={<SolarPower />} />
         <Route path="/wind" element={<WindPower />} />
+        <Route path="/marketing" element= {<Marketing/>}/>
+        <Route path="clients" element = {<Clients/>}/>
       </Routes>
     </Suspense>
   );
